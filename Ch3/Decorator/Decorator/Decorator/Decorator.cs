0818136ff913e2ff28ed7824df8d972cfed4ee13
Mingjing Yu <mingjingyu@microsoft.com>
2022-11-15 20:34:05 +0800

    .

---
+++ Ch3/Decorator/Decorator/Decorator/Decorator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    public abstract class Beverage
+    {
+        public string description = "Unknown Beverage";
+
+        public virtual string getDescription()
+        {
+            return description;
+        }
+
+        public abstract double cost();
+    }
+
+    public abstract class CondimentDecorator : Beverage
+    {
+    }
+
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            this.description = "Espresso";
+        }
+
+        public override double cost()
+        {
+            return 1.99;
+        }
+    }
+
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            this.description = "HouseBlend";
+        }
+
+        public override double cost()
+        {
+            return 0.89;
+        }
+    }
+
+    public class Mocha : CondimentDecorator
+    {
+        Beverage beverage;
+
+        public Mocha(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+
+        public override double cost()
+        {
+            return beverage.cost() + 0.2;
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ", Mocha";
+        }
+    }
+}
---
+++ Ch3/Decorator/Decorator/Decorator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Beverage beverage = new Espresso();
+            Console.WriteLine(beverage.getDescription() + " $ " + beverage.cost());
+
+            beverage = new HouseBlend();
+            Console.WriteLine(beverage.getDescription() + " $ " + beverage.cost());
+
+            beverage = new Mocha(beverage);
+            Console.WriteLine(beverage.getDescription() + " $ " + beverage.cost());
+
+            beverage = new Mocha(beverage);
+            Console.WriteLine(beverage.getDescription() + " $ " + beverage.cost());
+        }
+    }
+}
