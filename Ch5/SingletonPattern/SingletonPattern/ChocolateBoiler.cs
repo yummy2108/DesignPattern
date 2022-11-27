d8fef62beeed11d389dbfc922758328b63eeef36
Mingjing Yu <mingjingyu@microsoft.com>
2022-11-27 21:42:18 +0800

    Add ch5

---
+++ Ch5/SingletonPattern/SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class ChocolateBoiler
+    {
+        private static readonly Lazy<ChocolateBoiler> Lazy =
+            new Lazy<ChocolateBoiler>(() => new ChocolateBoiler());
+
+        public static ChocolateBoiler Instance => Lazy.Value;
+
+        private bool Empty;
+        private bool Boiled;
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public void Fill()
+        {
+            if (Empty)
+            {
+                Empty = false;
+                Boiled = false;
+                Console.WriteLine("Fill the boiler with a milk/chocolate mixture.");
+            }
+        }
+
+        public void Drain()
+        {
+            if (!Empty && Boiled)
+            {
+                Console.WriteLine("Drain the boiled milk and chocolate.");
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!Empty && !Boiled)
+            {
+                Console.WriteLine("Bring the contents to a boil.");
+                Boiled = true;
+            }
+        }
+    }
+}
--- Ch5/SingletonPattern/SingletonPattern/Program.cs
+++ Ch5/SingletonPattern/SingletonPattern/Program.cs
@@ -7,6 +7,27 @@ namespace SingletonPattern
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var boiler1 = ChocolateBoiler.Instance;
+            var boiler2 = ChocolateBoiler.Instance;
+            var boiler3 = ChocolateBoiler.Instance;
+            Console.Write("boiler1\n\t");
+            boiler1.Fill();
+            Console.Write("boiler3\n\t");
+            boiler3.Fill();
+            Console.Write("boiler1\n\t");
+            boiler1.Boil();
+            Console.Write("boiler2\n\t");
+            boiler2.Fill();
+            Console.Write("boiler2\n\t");
+            boiler2.Drain();
+            Console.Write("boiler2\n\t");
+            boiler2.Fill();
+            Console.Write("boiler1\n\t");
+            boiler1.Drain();
+            Console.Write("boiler1\n\t");
+            boiler1.Boil();
+            Console.Write("boiler2\n\t");
+            boiler2.Drain();
         }
     }
 }
