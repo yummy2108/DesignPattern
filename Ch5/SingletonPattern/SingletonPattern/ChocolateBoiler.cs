2977381b1db6547ac74ad80ead5469a1252f9f81
Mingjing Yu <mingjingyu@microsoft.com>
2022-11-27 21:47:58 +0800

    Add ch5

--- Ch5/SingletonPattern/SingletonPattern/ChocolateBoiler.cs
+++ Ch5/SingletonPattern/SingletonPattern/ChocolateBoiler.cs
@@ -6,10 +6,28 @@ namespace SingletonPattern
 {
     public class ChocolateBoiler
     {
-        private static readonly Lazy<ChocolateBoiler> Lazy =
-            new Lazy<ChocolateBoiler>(() => new ChocolateBoiler());
+        //private static readonly Lazy<ChocolateBoiler> Lazy =
+        //    new Lazy<ChocolateBoiler>(() => new ChocolateBoiler());
 
-        public static ChocolateBoiler Instance => Lazy.Value;
+        //public static ChocolateBoiler Instance => Lazy.Value;
+
+        private static ChocolateBoiler _uniqueInstance;
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (_uniqueInstance == null)
+                {
+                    Console.WriteLine("Creating unique instance of Chocolate Boiler");
+                    _uniqueInstance = new ChocolateBoiler();
+                }
+                else
+                {
+                    Console.WriteLine("Returning instance of Chocolate Boiler");
+                }
+                return _uniqueInstance;
+            }
+        }
 
         private bool Empty;
         private bool Boiled;
