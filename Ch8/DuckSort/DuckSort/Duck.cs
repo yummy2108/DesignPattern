306bc7c041b55e875cde390c42366228b7e97c58
Mingjing Yu Ⓜ️ <mingjingyu@microsoft.com>
2023-01-05 21:43:33 +0800

    .

---
+++ Ch8/DuckSort/DuckSort/Duck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckSort
+{
+    public class Duck : IComparable<Duck>
+    {
+        public string Name { get; }
+        public int Weight { get; }
+        public Duck(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public int CompareTo(Duck other)
+        {
+            if (Weight < other.Weight)
+            {
+                return -1;
+            }
+
+            return Weight == other.Weight ? 0 : 1;
+        }
+
+        public override string ToString() => $"{Name} weighs {Weight.ToString()}";
+
+    }
+}
---
+++ Ch8/DuckSort/DuckSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuckSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Duck[] ducks =
+            {
+                new Duck("Daffy", 8),
+                new Duck("Dewey", 2),
+                new Duck("Howard", 7),
+                new Duck("Louie", 2),
+                new Duck("Donald", 10),
+                new Duck("Huey", 2)
+            };
+
+            Console.WriteLine("Before sorting: ");
+            Display(ducks);
+
+            Array.Sort(ducks);
+
+            Console.WriteLine("After sorting:");
+            Display(ducks);
+        }
+
+        public static void Display(IEnumerable<Duck> ducks)
+        {
+            foreach (var duck in ducks)
+            {
+                Console.WriteLine(duck);
+            }
+        }
+    }
+}
